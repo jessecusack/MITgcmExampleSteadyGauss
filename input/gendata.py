ee5950abb30f8700137409ba6d44b2f18364fa44
Jesse Cusack <jesse.cusack@noc.soton.ac.uk>
2018-06-28 16:33:48 -0700

    cleaned up gendata a bit

--- input/gendata.py
+++ input/gendata.py
@@ -1,34 +1,35 @@
-from numpy import *
-from scipy import *
-from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
 import numpy.matlib as matlib
-from shutil import copy
-from os import mkdir
+import shutil
 import os
 
-def lininc(n,Dx,dx0):
-  a=(Dx-n*dx0)*2./n/(n+1)
-  dx = dx0+arange(1.,n+1.,1.)*a
-  return dx
 
-Fr=0.13
+def lininc(n, Dx, dx0):
+    a = (Dx-n*dx0)*2./n/(n+1)
+    dx = dx0 + np.arange(1., n+1., 1.)*a
+    return dx
+
+
+Fr = 0.13
 H = 2000.
-h0=500.
-om = 2.*pi/12.42/3600.
-N0=5.2e-3
-u0=Fr*N0*h0
+h0 = 500.
+om = 2.*np.pi/12.42/3600.
+N0 = 5.2e-3
+u0 = Fr*N0*h0
 
 
-outdir='../runs/RunFr%03d' % (10000*Fr)
+outdir = '../runs/RunFr{:1.3f}'.format(Fr)
 try:
-  mkdir(outdir)
+    os.mkdir(outdir)
 except:
-  print( outdir+' Exists')
+    print(outdir+' Exists')
 try:
-  mkdir(outdir+'/figs')
+    os.mkdir(outdir+'/figs')
 except:
-  print(outdir+'/figs Exists')
-copy('./gendata.py',outdir)
+    print(outdir+'/figs Exists')
+
+shutil.copy('./gendata.py', outdir)
 
 # These must match ../code/SIZE.h
 ny = 1
@@ -41,134 +42,132 @@ dy = 1000
 xt = 410e3
 
 nmid = 50
-dx0=300.
+dx0 = 300.
 nleft = int((nx-nmid)/2)
 print(nleft)
 nright = int((nx-nmid)/2)
-dx = zeros(nx)
-dxleft = flipud(lininc(nleft,200.e3,dx0))
-dxright = lininc(nright,200.e3,dx0)
-dx[0:nleft]=dxleft
-dx[(nleft):(nleft+nmid)]=dx0
-dx[(nleft+nmid):]=dxright
-x=cumsum(dx)
-x = x-x[int(np.floor(nx/2))]
+dx = np.zeros(nx)
+dxleft = np.flipud(lininc(nleft, 200.e3, dx0))
+dxright = lininc(nright, 200.e3, dx0)
+dx[0:nleft] = dxleft
+dx[(nleft):(nleft+nmid)] = dx0
+dx[(nleft+nmid):] = dxright
+x = np.cumsum(dx)
+x = x - x[int(np.floor(nx/2))]
 
 with open(outdir+"/delXvar.bin", "wb") as f:
-	dx.tofile(f)
-f.close()
+    dx.tofile(f)
+
 # plot
-if 1:
-    plot(x/1000.,dx)
-    xlim([-10,10])
-    savefig(outdir+'/figs/dx.pdf')
+if True:
+    plt.figure()
+    plt.plot(x/1000., dx)
+    plt.xlim([-10, 10])
+    plt.savefig(outdir+'/figs/dx.pdf')
 
 # topo
-sigma = 4000. # m
+sigma = 4000.  # m
 
-topo = 1500*exp(-x*x/(sigma**2))-1500+h0
-#topo = h0*exp(-x*x/(3000**2))
-print(shape(topo))
-topo[topo<0.]=0.
-topo=-H+topo
-topo[topo<-H]=-H
+topo = 1500*np.exp(-x*x/(sigma**2)) - 1500 + h0
+# topo = h0*exp(-x*x/(3000**2))
+print(topo.shape)
+topo[topo < 0.] = 0.
+topo = -H + topo
+topo[topo < -H] = -H
 
 # plot
-if 1:
-    clf()
-    plot(x/1.e3,topo)
-    # xlim([-20.,20.])
-    savefig(outdir+'/figs/topo.pdf')
+if True:
+    plt.figure()
+    plt.plot(x/1.e3, topo)
+    # plt.xlim([-20.,20.])
+    plt.savefig(outdir+'/figs/topo.pdf')
 
 
 with open(outdir+"/topo.bin", "wb") as f:
-	topo.tofile(f)
-f.close()
+    topo.tofile(f)
+
 # dz:
 # dz is from the surface down (right?).  Its saved as positive.
 
-dz=zeros(nz)+H/nz
+dz = np.zeros(nz) + H/nz
 
 with open(outdir+"/delZvar.bin", "wb") as f:
-	dz.tofile(f)
-f.close()
+    dz.tofile(f)
+
 
 # temperature profile...
-g=9.8
+g = 9.8
 alpha = 2e-4
-T0 = 28+cumsum(N0**2/g/alpha*(-dz))
+T0 = 28 + np.cumsum(N0**2/g/alpha*(-dz))
 
 with open(outdir+"/TRef.bin", "wb") as f:
-	T0.tofile(f)
-f.close()
+    T0.tofile(f)
+
 
 # save T0 over whole domain
-TT0 = matlib.repmat(T0,nx,1).T
+TT0 = matlib.repmat(T0, nx, 1).T
 with open(outdir+"/T0.bin", "wb") as f:
-	TT0.tofile(f)
+    TT0.tofile(f)
 
 
-z=cumsum(dz)
+z = np.cumsum(dz)
 # plot:
-if 1:
-    clf()
-    plot(T0,z)
-    savefig(outdir+'/figs/TO.pdf')
+if True:
+    plt.figure()
+    plt.plot(T0, z)
+    plt.savefig(outdir+'/figs/TO.pdf')
 
 # Forcing for boundaries
-dt=3720.
-time = arange(0,12.*3720.,dt)
-print( time/3600./12.4)
-om = 2*pi/12.40/3600;
+dt = 3720.
+time = np.arange(0, 12.*3720., dt)
+print(time/3600./12.4)
+om = 2*np.pi/12.40/3600
 uw = u0+0.*time
 ue = u0+0.*time
 # plot:
-if 1:
-    clf()
-    plot(time/3600./12.4,ue,label='Ue')
-    plot(time/3600/12.4,uw,label='Uw')
-    legend()
-    xlabel('t/T')
-    ylabel('Vel')
-    title('%d' % time[-1])
-    savefig(outdir+'/figs/Vels.pdf')
+if True:
+    plt.figure()
+    plt.plot(time/3600./12.4, ue, label='Ue')
+    plt.plot(time/3600/12.4, uw, label='Uw')
+    plt.legend()
+    plt.xlabel('t/T')
+    plt.ylabel('Vel')
+    plt.title('{}'.format(time[-1]))
+    plt.savefig(outdir+'/figs/Vels.pdf')
 
 # try time,nz,ny...
 
-uen=zeros((shape(time)[0],nz,ny))
-for j in range(0,ny):
-  for i in range(0,nz):
-    uen[:,i,j]=ue
-#print(uen)
+uen = np.zeros((time.shape[0], nz, ny))
+for j in range(0, ny):
+    for i in range(0, nz):
+        uen[:, i, j] = ue
 
-uwn=zeros((shape(time)[0],nz,ny))
-print(shape(uwn))
-for j in range(0,ny):
-  for i in range(0,nz):
-    uwn[:,i,j]=uw
-#print(uwn)
+uwn = np.zeros((time.shape[0], nz, ny))
+print(uwn.shape)
+for j in range(0, ny):
+    for i in range(0, nz):
+        uwn[:, i, j] = uw
 
-with open(outdir+"/Ue.bin","wb") as f:
-  uen.tofile(f)
+with open(outdir+"/Ue.bin", "wb") as f:
+    uen.tofile(f)
 
 with open(outdir+"/Uw.bin", "wb") as f:
-  uwn.tofile(f)
+    uwn.tofile(f)
 
-t=zeros((shape(time)[0],nz,ny))
-for j in range(0,ny):
-	for i in range(0,nz):
-		for k in range(0,shape(time)[0]):
-			t[k,i,j]=T0[i]
-print(shape(t))
+t = np.zeros((time.shape[0], nz, ny))
+for j in range(0, ny):
+    for i in range(0, nz):
+        for k in range(0, time.shape[0]):
+            t[k, i, j] = T0[i]
+
+print(t.shape)
 with open(outdir+"/Te.bin", "wb") as f:
-	t.tofile(f)
-f.close()
-with open(outdir+"/Tw.bin", "wb") as f:
-	t.tofile(f)
-f.close()
+    t.tofile(f)
 
-## Copy some other files
-import shutil
+with open(outdir+"/Tw.bin", "wb") as f:
+    t.tofile(f)
+
+# Copy some other files
 shutil.copy('data', outdir+'/data')
 shutil.copy('eedata', outdir)
 shutil.copy('data.kl10', outdir)
@@ -177,7 +176,8 @@ shutil.copy('data.obcs', outdir)
 shutil.copy('data.diagnostics', outdir)
 shutil.copy('data.pkg', outdir+'/data.pkg')
 # also store these.  They are small and helpful to document what we did
-for nm in {'input','code','build_options','analysis'}:
+
+for nm in {'input', 'code', 'build_options', 'analysis'}:
     to_path = outdir+'/'+nm
     if os.path.exists(to_path):
         shutil.rmtree(to_path)
